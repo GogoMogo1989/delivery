771e106b8180238be8a0981179816a518e25f64c
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-09-08 13:59:13 +0200

    20220908

---
+++ src/routes/Routers.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home.jsx'
+import AllFoods from '../pages/AllFoods'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+import Contact from '../pages/Contact'
+import FoodDetails from '../pages/FoodDetails'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+const Routers = () => {
+  return <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/foods' element={<AllFoods />} />
+        <Route path='/foods:id' element={<FoodDetails />} /> 
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/contact' element={<Contact />} />
+  </Routes>
+}
+
+export default Routers
